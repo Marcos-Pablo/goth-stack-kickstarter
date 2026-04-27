0a5e3b34ddaff6e66896b9af1df6c0766f2547ed
Marcos Pablo <marquin.pablo@gmail.com>
2026-04-27 02:05:54 -0300

    chore(views): move types to a central file

--- views/types.go
+++ views/types.go
@@ -1,6 +1,23 @@
 package views
 
+import "github.com/Marcos-Pablo/goth-stack-kickstarter/internal/validator"
 type User struct {
 	Email string
 	Name  string
+}
+
+type SignInForm struct {
+	Email        string
+	Password     string
+	GeneralError string
+	validator.Validator
+}
+
+type SignUpForm struct {
+	Email                string
+	Name                 string
+	Password             string
+	PasswordConfirmation string
+	GeneralError         string
+	validator.Validator
 }
